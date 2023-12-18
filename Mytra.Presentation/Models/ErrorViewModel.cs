bab8586d3a585d2f01a4919cd82de88798dc1d7d
Ilyas Bural <ilyasbural@gmail.com>
2023-12-18 12:09:19 +0300

    Presentation layer has been created

---
+++ Mytra.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mytra.Presentation.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
