d1e092ad7c9bacd4345ec8f6c77709f052d5cd01
ilyasbural <ilyasbural@gmail.com>
2025-02-20 08:08:52 -0800

    common layer has been created

---
+++ Mytra.Common/Response/Response.cs
@@ -0,0 +1,16 @@
+﻿namespace Mytra.Common
+{
+    public class Response<T>
+    {
+        public T ResponseData { get; set; } = default(T)!;
+        public List<T> ResponseDataSource { get; set; } = new List<T>();
+        //public List<ValidationError> Errors { get; set; } = null!;
+        //public AuthenticationInfo Authentication { get; set; } = new();
+        public int Success { get; set; }
+
+        public Response()
+        {
+
+        }
+    }
+}
---
+++ Mytra.Common/Response/UserAuthenticationResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Mytra.Common
+{
+	public class UserAuthenticationResponse : Response<UserAuthenticationResponse>
+	{
+
+	}
+}
