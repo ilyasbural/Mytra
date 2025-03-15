29b1dcdb097fe419561e7e985c9defef2bd39588
ilyas bural <ilyasbural@gmail.com>
2025-03-16 00:27:17 +0300

    userauthenticationmapping code changed

--- Mytra.DataAccess/Mapping/UserAuthenticationMapping.cs
+++ Mytra.DataAccess/Mapping/UserAuthenticationMapping.cs
@@ -1,18 +1,18 @@
 ﻿namespace Mytra.DataAccess
 {
-	using Core;
-	using Microsoft.EntityFrameworkCore;
-	using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Core;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-	public class UserAuthenticationMapping : IEntityTypeConfiguration<UserAuthentication>
-	{
-		public void Configure(EntityTypeBuilder<UserAuthentication> builder)
-		{
-			builder.Property(x => x.Id).HasColumnOrder(0);
-			builder.Property(x => x.RegisterDate).HasColumnType("DATETIME").IsRequired(true).HasColumnOrder(1);
-			builder.Property(x => x.UpdateDate).HasColumnType("DATETIME").IsRequired(true).HasColumnOrder(2);
-			builder.Property(x => x.IsActive).HasColumnType("BIT").IsRequired(true).HasColumnOrder(3);
-			builder.ToTable("UserAuthentication");
-		}
-	}
+    public class UserAuthenticationMapping : IEntityTypeConfiguration<UserAuthentication>
+    {
+        public void Configure(EntityTypeBuilder<UserAuthentication> builder)
+        {
+            builder.Property(x => x.Id).HasColumnOrder(0);
+            builder.Property(x => x.RegisterDate).HasColumnType("DATETIME").IsRequired(true).HasColumnOrder(1);
+            builder.Property(x => x.UpdateDate).HasColumnType("DATETIME").IsRequired(true).HasColumnOrder(2);
+            builder.Property(x => x.IsActive).HasColumnType("BIT").IsRequired(true).HasColumnOrder(3);
+            builder.ToTable("UserAuthentication");
+        }
+    }
 }
