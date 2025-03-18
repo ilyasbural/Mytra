49dd927968dbae74a10e38e5b3500ed4d90eeef7
ilyas bural <ilyasbural@gmail.com>
2025-03-18 08:39:22 +0300

    language, jobposting restapi services have been created

---
+++ Mytra.Presentation/Controllers/JobPostingDetailController.cs
@@ -0,0 +1,13 @@
+﻿namespace Mytra.Presentation.Controllers
+{
+	using Core;
+	using Common;
+	using Microsoft.AspNetCore.Mvc;
+
+	[Route("api/[controller]")]
+    [ApiController]
+    public class JobPostingDetailController : ControllerBase
+    {
+
+    }
+}
