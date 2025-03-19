de035a6217691012268660e67b9c7bcac1f07c6e
ilyas bural <ilyasbural@gmail.com>
2025-03-20 00:39:11 +0300

    iunitofwork interfaces edited

--- Mytra.Core/Interface/IUnitOfWork.cs
+++ Mytra.Core/Interface/IUnitOfWork.cs
@@ -5,14 +5,31 @@
 		ICandidate Candidate { get; }
 		ICandidateAuthentication Authentication { get; }
 		ICandidateCertificate Certificate { get; }
-		ICollege College { get; }
+        ICandidateContact CandidateContact { get; }
+        ICandidateDetail CandidateDetail { get; }
+        ICandidateEducation CandidateEducation { get; }
+        ICandidateExperience CandidateExperience { get; }
+        ICandidateLanguage CandidateLanguage { get; }
+        ICandidatePhoto CandidatePhoto { get; }
+        ICandidateReferance CandidateReferance { get; }
+        ICandidateSettings CandidateSettings { get; }
+        ICandidateSkills CandidateSkills { get; }
+        ICollege College { get; }
 		IJobPostingApply JobPostingApply { get; }
 		IJobPostingDetail JobPostingDetail { get; }
 		IJobPosting JobPosting { get; }
-		ILanguage Language { get; }		
-		IUserDetail UserDetail { get; }
-		IUser User { get; }
-		IUserSettings UserSettings { get; }
+        IJobPostingVisit JobPostingVisit { get; }
+        IInstitution Institution { get; }
+        ILanguage Language { get; }
+        IManager Manager { get; }
+        IManagerAuthentication ManagerAuthentication { get; }
+        IManagerDetail ManagerDetail { get; }
+        IManagerSettings ManagerSettings { get; }
+        ISkills Skills { get; }
+        IUser User { get; }
+        IUserAuthentication UserAuthentication { get; }
+        IUserDetail UserDetail { get; }
+        IUserSettings UserSettings { get; }
 		Task<int> SaveChangesAsync();
     }
 }
