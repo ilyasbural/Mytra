bc47ac8413604787ea29abc63ac85427e4d25ee3
ilyas bural <ilyasbural@gmail.com>
2025-04-03 18:31:41 +0300

    yeni

--- Mytra.Core/Interface/IRepository.cs
+++ Mytra.Core/Interface/IRepository.cs
@@ -7,7 +7,6 @@
     public interface IRepository<T> where T : class, IEntity, new()
     {
         Task InsertAsync(T Entity);
-        Task InsertRangeAsync(List<T> Entities);
         Task UpdateAsync(T Entity);
         Task DeleteAsync(T Entity);
         Task<List<T>> SelectAsync(Expression<Func<T, bool>> Predicate, params Expression<Func<T, object>>[] IncludeProperties);
