f22d7be1fefa66717667613b6987c482ff025d6e
ilyasbural <ilyasbural@gmail.com>
2025-08-26 13:57:40 +0300

    ICandidateAuthentication interface described

--- Mytra.Core/Interface/Service/ICandidateAuthenticationService.cs
+++ Mytra.Core/Interface/Service/ICandidateAuthenticationService.cs
@@ -2,6 +2,10 @@
 {
     public interface ICandidateAuthenticationService
     {
-
-    }
+		//Task<Common.ServiceResponse<Common.CandidateAuthenticationResponse>> InsertAsync(AbilityRegisterDto Model);
+		//Task<Common.ServiceResponse<Common.CandidateAuthenticationResponse>> UpdateAsync(AbilityUpdateDto Model);
+		//Task<Common.ServiceResponse<Common.CandidateAuthenticationResponse>> DeleteAsync(AbilityDeleteDto Model);
+		//Task<Common.ServiceResponse<Common.CandidateAuthenticationResponse>> SelectAsync(AbilitySelectDto Model);
+		//Task<Common.ServiceResponse<Common.CandidateAuthenticationResponse>> SelectSingleAsync(AbilitySelectDto Model);
+	}
 }
