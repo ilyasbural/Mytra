3993a320b33a49d5d7d75b0f6aa37e130fb11f8f
ilyasbural <ilyasbural@gmail.com>
2025-08-27 19:32:44 +0300

    CandidateSkillsService

--- Mytra.Service/Service/CandidateSkillsService.cs
+++ Mytra.Service/Service/CandidateSkillsService.cs
@@ -1,9 +1,38 @@
 ﻿namespace Mytra.Service
 {
 	using Core;
+	using Common;
 
 	public class CandidateSkillsService : ICandidateSkillsService
 	{
+		public CandidateSkillsService()
+		{
+			
+		}
 
+		public async Task<ServiceResponse<CandidateSkillsResponse>> InsertAsync(CandidateSkillsInsert Model)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<ServiceResponse<CandidateSkillsResponse>> UpdateAsync(CandidateSkillsUpdate Model)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<ServiceResponse<CandidateSkillsResponse>> DeleteAsync(CandidateSkillsDelete Model)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<ServiceResponse<CandidateSkillsResponse>> SelectAsync(CandidateSkillsSelect Model)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<ServiceResponse<CandidateSkillsResponse>> SelectSingleAsync(CandidateSkillsSelectSingle Model)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
