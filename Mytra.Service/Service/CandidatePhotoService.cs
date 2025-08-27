eb70296658d7fdd83d2fa94ee3103d8504af6257
ilyasbural <ilyasbural@gmail.com>
2025-08-27 17:45:36 +0300

    CandidatePhotoService

--- Mytra.Service/Service/CandidatePhotoService.cs
+++ Mytra.Service/Service/CandidatePhotoService.cs
@@ -1,9 +1,38 @@
 ﻿namespace Mytra.Service
 {
 	using Core;
+	using Common;
 
 	public class CandidatePhotoService : ICandidatePhotoService
 	{
+		public CandidatePhotoService()
+		{
+			
+		}
 
+		public async Task<ServiceResponse<CandidatePhotoResponse>> InsertAsync(CandidatePhotoInsert Model)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<ServiceResponse<CandidatePhotoResponse>> UpdateAsync(CandidatePhotoUpdate Model)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<ServiceResponse<CandidatePhotoResponse>> DeleteAsync(CandidatePhotoDelete Model)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<ServiceResponse<CandidatePhotoResponse>> SelectAsync(CandidatePhotoSelect Model)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<ServiceResponse<CandidatePhotoResponse>> SelectSingleAsync(CandidatePhotoSelectSingle Model)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
