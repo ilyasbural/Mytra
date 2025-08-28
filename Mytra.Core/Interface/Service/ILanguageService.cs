dbaf8161ad3176371f155f246823074ee54b7471
ilyasbural <ilyasbural@gmail.com>
2025-08-28 11:40:08 +0300

    LanguageService

--- Mytra.Core/Interface/Service/ILanguageService.cs
+++ Mytra.Core/Interface/Service/ILanguageService.cs
@@ -2,6 +2,10 @@
 {
     public interface ILanguageService
     {
-
-    }
+		Task<Common.ServiceResponse<Common.LanguageResponse>> InsertAsync(Common.LanguageInsert Model);
+		Task<Common.ServiceResponse<Common.LanguageResponse>> UpdateAsync(Common.LanguageUpdate Model);
+		Task<Common.ServiceResponse<Common.LanguageResponse>> DeleteAsync(Common.LanguageDelete Model);
+		Task<Common.ServiceResponse<Common.LanguageResponse>> SelectAsync(Common.LanguageSelect Model);
+		Task<Common.ServiceResponse<Common.LanguageResponse>> SelectSingleAsync(Common.LanguageSelectSingle Model);
+	}
 }
--- Mytra.Service/Service/LanguageService.cs
+++ Mytra.Service/Service/LanguageService.cs
@@ -1,9 +1,38 @@
 ﻿namespace Mytra.Service
 {
 	using Core;
+	using Common;
 
 	public class LanguageService : ILanguageService
 	{
+		public LanguageService()
+		{
+			
+		}
 
+		public async Task<ServiceResponse<LanguageResponse>> InsertAsync(LanguageInsert Model)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<ServiceResponse<LanguageResponse>> UpdateAsync(LanguageUpdate Model)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<ServiceResponse<LanguageResponse>> DeleteAsync(LanguageDelete Model)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<ServiceResponse<LanguageResponse>> SelectAsync(LanguageSelect Model)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<ServiceResponse<LanguageResponse>> SelectSingleAsync(LanguageSelectSingle Model)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
