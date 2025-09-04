d7aace83a11a867bc216190db311c75aa5c0b6df
ilyasbural <ilyasbural@gmail.com>
2025-09-05 01:13:18 +0300

    library

--- Mytra.UI/Program.cs
+++ Mytra.UI/Program.cs
@@ -1,11 +1,9 @@
+using Mytra.UI;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Mytra.UI;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 await builder.Build().RunAsync();
