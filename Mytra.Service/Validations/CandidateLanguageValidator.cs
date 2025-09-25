c8fff2c8baa321d60d5aed65ec4dfc7b4ad8c1b8
ilyasbural <ilyasbural@gmail.com>
2025-09-26 01:33:37 +0300

    Validation classes have been created

---
+++ Mytra.Service/Validations/CandidateLanguageValidator.cs
@@ -0,0 +1,15 @@
+﻿namespace Mytra.Service
+{
+	using Core;
+	using FluentValidation;
+
+	public class CandidateLanguageValidator : AbstractValidator<CandidateLanguage>
+	{
+		public CandidateLanguageValidator()
+		{
+			RuleFor(x => x.RegisterDate).NotEmpty().WithMessage("RegisterDate can not be null");
+			RuleFor(x => x.UpdateDate).NotEmpty().WithMessage("UpdateDate can not be null");
+			RuleFor(x => x.IsActive).NotEmpty().WithMessage("IsActive can not be null");
+		}
+	}
+}
---
+++ Mytra.Service/Validations/CandidateValidator.cs
@@ -0,0 +1,16 @@
+﻿namespace Mytra.Service
+{
+	using Core;
+	using FluentValidation;
+
+	public class CandidateValidator : AbstractValidator<Candidate>
+	{
+		public CandidateValidator()
+		{
+			//RuleFor(x => x.Name).NotEmpty().WithMessage("Name can not be null");
+			RuleFor(x => x.RegisterDate).NotEmpty().WithMessage("RegisterDate can not be null");
+			RuleFor(x => x.UpdateDate).NotEmpty().WithMessage("UpdateDate can not be null");
+			RuleFor(x => x.IsActive).NotEmpty().WithMessage("IsActive can not be null");
+		}
+	}
+}
