03bd662b9909452dfbf57bffa87d6bf60ef8dd08
ilyasbural <ilyasbural@gmail.com>
2025-09-25 23:25:07 +0300

    JobPostingVisitRepositoryEF JobPostingRepositoryEF JobPostingDetailRepositoryEF JobPostingApplyRepositoryEF repositories

---
+++ Mytra.DataAccess/Repositories/JobPostingApplyRepositoryEF.cs
@@ -0,0 +1,10 @@
+﻿namespace Mytra.DataAccess
+{
+	public class JobPostingApplyRepositoryEF : RepositoryBase<Core.JobPostingApply>, Core.IJobPostingApply
+	{
+		public JobPostingApplyRepositoryEF(Microsoft.EntityFrameworkCore.DbContext dbContext) : base(dbContext)
+		{
+
+		}
+	}
+}
---
+++ Mytra.DataAccess/Repositories/JobPostingVisitRepositoryEF.cs
@@ -0,0 +1,10 @@
+﻿namespace Mytra.DataAccess
+{
+	public class JobPostingVisitRepositoryEF : RepositoryBase<Core.JobPostingVisit>, Core.IJobPostingVisit
+	{
+		public JobPostingVisitRepositoryEF(Microsoft.EntityFrameworkCore.DbContext dbContext) : base(dbContext)
+		{
+
+		}
+	}
+}
