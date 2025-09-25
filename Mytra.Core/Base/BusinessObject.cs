3b0c09c52ada52115dc2892ceb331ffec6028eea
ilyasbural <ilyasbural@gmail.com>
2025-09-25 18:15:44 +0300

    Base, BusinessObject Classes have been created

---
+++ Mytra.Core/Base/BusinessObject.cs
@@ -0,0 +1,10 @@
+﻿namespace Mytra.Core
+{
+    public class BusinessObject<T>
+    {
+        protected T Data { get; set; } = default!;
+        protected List<T> Collection { get; set; } = null!;
+        protected bool Success { get; set; }
+        public BusinessObject() { }
+    }
+}
