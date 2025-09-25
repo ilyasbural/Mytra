18eff140a5f8ec6d5ffed6324063325be8335037
ilyasbural <ilyasbural@gmail.com>
2025-09-25 20:32:45 +0300

    DataService, Response, ServiceResponse, RefreshTokenGenerator have been created

---
+++ Mytra.Utilize/RefreshTokenGenerator.cs
@@ -0,0 +1,15 @@
+﻿namespace Mytra.Utilize
+{
+	public class RefreshTokenGenerator
+	{
+		public string GenerateRefreshToken()
+		{
+			Byte[] randomNumber = new byte[32];
+			using (var Rng = System.Security.Cryptography.RandomNumberGenerator.Create())
+			{
+				Rng.GetBytes(randomNumber);
+				return Convert.ToBase64String(randomNumber);
+			}
+		}
+	}
+}
---
+++ Mytra.Utilize/Response.cs
@@ -0,0 +1,10 @@
+﻿namespace Mytra.Utilize
+{
+	public abstract class Response<T> where T : class
+	{
+		public Guid Id { get; set; }
+		public DateTime RegisterDate { get; set; }
+		public DateTime UpdateDate { get; set; }
+		public bool IsActive { get; set; }
+	}
+}
