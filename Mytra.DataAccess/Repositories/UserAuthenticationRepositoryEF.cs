75f240ac0a118ba7bc4dab659a4e519e12ce6263
ilyasbural <ilyasbural@gmail.com>
2025-09-25 23:22:57 +0300

    UserAuthenticationRepositoryEF, UserDetailRepositoryEF, UserRepositoryEF, UserSettingsRepositoryEF repositories created

---
+++ Mytra.DataAccess/Repositories/UserAuthenticationRepositoryEF.cs
@@ -0,0 +1,10 @@
+﻿namespace Mytra.DataAccess
+{
+	public class UserAuthenticationRepositoryEF : RepositoryBase<Core.UserAuthentication>, Core.IUserAuthentication
+	{
+		public UserAuthenticationRepositoryEF(Microsoft.EntityFrameworkCore.DbContext dbContext) : base(dbContext)
+		{
+
+		}
+	}
+}
